41f00b61bc14c8925b1704f12c5efcd9dc728515
pinkachu42 <pinkachu42@protonmail.com>
2025-11-23 19:40:15 +0700

    implemented bootdev instructions

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    with open(path) as f:
+        text = f.read()
+
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    sorted_chars = chars_dict_to_sorted_list(chars_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
+    print("============= END ===============")
+
+if __name__ == "__main__":
+    main()
